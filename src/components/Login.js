7cd6cbc0e9029799fc89d22fb7afa3b08d431853
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2020-01-25 20:49:01 +0100

    Login / Register components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,129 +1,136 @@
-import React from 'react';
+import React from "react";
 
 import {
-    NavLink, 
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    FormGroup,
-    Form,
-    Col,
-    Label,
-    Input,
-    Button
-  } from "reactstrap";
+  NavLink,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  FormGroup,
+  Form,
+  Col,
+  Label,
+  Input,
+  Button
+} from "reactstrap";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleLoginShow = this.handleLoginShow.bind(this);
-        this.handleLoginClose = this.handleLoginClose.bind(this);
-    
-        this.state = {
-          emailUser: "",
-          passwordUser: "",
-          showLogin: false
+    this.toggleModal = this.toggleModal.bind(this);
+    this.onChange = this.onChange.bind(this);
 
-        };
-    }
+    this.state = {
+      emailUser: "",
+      passwordUser: "",
+      showLogin: false
+    };
+  }
 
-    handleLoginShow() {
-        console.log("handle login show called");
-        this.setState({ showLogin: true});
-      }
-    
-      handleLoginClose() {
-        this.setState({ showLogin: false});
-    
-      }
-    
-    render() { 
-        return ( 
-            <div>
-                <div>
-                    <p>Log</p>
-                    <NavLink
-                    className="btn"
-                    style={buttonStyle}
-                    onClick={this.handleLoginShow}
-                    >
-                    Log In
-                    </NavLink>       
-                </div>
-                <div className="someStrangeModal">
-                    <Modal 
-                    isOpen={this.state.showLogin}
-                    > 
-                        <ModalHeader toggle={this.props.onClose} >Log In</ModalHeader>
+  toggleModal() {
+    this.setState({
+      showLogin: !this.state.showLogin
+    });
+  }
 
-                        <ModalBody>
-                            <Form>
-                                <FormGroup id="userNameHidden" row style={nameHidden}>
-                                    <Label sm={2}>Name</Label>
+  modalButtonSubmit = () => {
+    this.toggleModal();
+  };
 
-                                    <Col sm={10}>
-                                        <Input
-                                        name="loginUser"
-                                        id="loginUserLog"
-                                        type="text"
-                                        placeholder="Name"
-                                        autoComplete="username"
-                                        />
-                                    </Col>
-                                    </FormGroup>
+  onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-                                    <FormGroup row>
-                                    <Label sm={2}>Email</Label>
+    this.setState({
+      [name]: value
+    });
+    console.log(value);
+    e.preventDefault();
+  };
 
-                                    <Col sm={10}>
-                                        <Input
-                                        name="emailUser"
-                                        type="emil"
-                                        placeholder="Email"
-                                        autoComplete="email"
-                                        />
-                                    </Col>
-                                    </FormGroup>
+  render() {
+    return (
+      <div>
+        <NavLink
+          className="btn"
+          style={buttonStyle}
+          onClick={() => this.toggleModal()}
+        >
+          Log In
+        </NavLink>
 
-                                    <FormGroup row>
-                                    <Label sm={2}>Password</Label>
+        <Modal isOpen={this.state.showLogin} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Log In</ModalHeader>
 
-                                    <Col sm={10}>
-                                        <Input
-                                        name="passwordUser"
-                                        type="password"
-                                        placeholder="Password"
-                                        autoComplete="current-password"
-                                        />
-                                    </Col>
-                                </FormGroup>
-                            </Form>
-                        </ModalBody>
-                        <ModalFooter>
-                        <Button 
-                        color="success" 
-                        type="submit"
-                        onClick={ this.props.onClose}
-                        >
-                            Log In
-                        </Button>
-                        </ModalFooter>
-                    </Modal>
-                </div>
-            </div>
-         );
-    }
+          <ModalBody>
+            <Form>
+              <FormGroup id="userNameHidden" row style={nameHidden}>
+                <Label sm={2}>Name</Label>
+
+                <Col sm={10}>
+                  <Input
+                    name="loginUser"
+                    id="loginUserLog"
+                    type="text"
+                    placeholder="Name"
+                    autoComplete="username"
+                  />
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label sm={2}>Email</Label>
+
+                <Col sm={10}>
+                  <Input
+                    name="emailUser"
+                    type="emil"
+                    placeholder="Email"
+                    autoComplete="email"
+                    value={this.state.emailUser}
+                    onChange={this.onChange}
+                  />
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label sm={2}>Password</Label>
+
+                <Col sm={10}>
+                  <Input
+                    name="passwordUser"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="current-password"
+                    value={this.state.passwordUser}
+                    onChange={this.onChange}
+                  />
+                </Col>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="success"
+              type="submit"
+              onClick={this.modalButtonSubmit}
+            >
+              Log In
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 const buttonStyle = {
-    color: "#fff"
-  };
+  color: "#fff"
+};
 
 const nameHidden = {
-    display: "none"
-  };
-  
- 
+  display: "none"
+};
+
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,127 +1,141 @@
-import React from 'react';
+import React from "react";
 
 import {
-    NavLink, 
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    FormGroup,
-    Form,
-    Col,
-    Label,
-    Input,
-    Button
-  } from "reactstrap";
+  NavLink,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  FormGroup,
+  Form,
+  Col,
+  Label,
+  Input,
+  Button
+} from "reactstrap";
 
 class Register extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          loginUser: "",
-          emailUser: "",
-          passwordUser: ""
-        };
-    
-        this.onChange = this.onChange.bind(this);
-      }
+  constructor(props) {
+    super(props);
 
-    onChange = e => {
-        const inputName = e.target.name;
-        const value = e.target.value;
+    this.toggleModalRegister = this.toggleModalRegister.bind(this);
+    this.onChange = this.onChange.bind(this);
 
-        this.setState({
-            [inputName]: value
-        });
-
-        e.preventDefault();
+    this.state = {
+      loginUser: "",
+      emailUser: "",
+      passwordUser: "",
+      showRegister: false
     };
+  }
 
+  toggleModalRegister() {
+    this.setState({
+      showRegister: !this.state.showRegister
+    });
+  }
 
+  modalButtonRegister = () => {
+    this.toggleModalRegister();
+  };
 
+  onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-    render() { 
-        return ( 
-            <div>
-                <p>Reg</p>
+    this.setState({
+      [name]: value
+    });
 
-                <NavLink
-                  className="btn"
-                  style={buttonStyle}
-                >
-                  Register
-                </NavLink>
+    e.preventDefault();
+  };
 
-                <div>
-                    <Modal>
-                        <ModalHeader>Register</ModalHeader>
+  render() {
+    return (
+      <div>
+        <NavLink
+          className="btn"
+          style={buttonStyle}
+          onClick={() => this.toggleModalRegister()}
+        >
+          Register
+        </NavLink>
 
-                        <ModalBody>
-                            <Form>
-                                
-                                <FormGroup row>
-                                    <Label sm={2}>Name</Label>
+        <Modal
+          isOpen={this.state.showRegister}
+          toggle={this.toggleModalRegister}
+        >
+          <ModalHeader toggle={this.toggleModalRegister}>Register</ModalHeader>
 
-                                    <Col sm={10}>
-                                        <Input
-                                        name="loginUser"
-                                        id="loginUser"
-                                        type="text"
-                                        placeholder="Name"
-                                        autoComplete="username"
-                                        onChange={this.onChange}
-                                        />
-                                    </Col>
-                                </FormGroup>
+          <ModalBody>
+            <Form>
+              <FormGroup row>
+                <Label sm={2}>Name</Label>
 
-                                <FormGroup row>
-                                    <Label sm={2}>Email</Label>
+                <Col sm={10}>
+                  <Input
+                    name="loginUser"
+                    id="loginUser"
+                    type="text"
+                    placeholder="Name"
+                    autoComplete="username"
+                    value={this.state.loginUser}
+                    onChange={this.onChange}
+                  />
+                </Col>
+              </FormGroup>
 
-                                    <Col sm={10}>
-                                        <Input
-                                        name="emailUser"
-                                        id="emailUser"
-                                        type="emil"
-                                        placeholder="Email"
-                                        autoComplete="email"
-                                        onChange={this.onChange}
+              <FormGroup row>
+                <Label sm={2}>Email</Label>
 
-                                        />
-                                    </Col>
-                                </FormGroup>
+                <Col sm={10}>
+                  <Input
+                    name="emailUser"
+                    id="emailUser"
+                    type="emil"
+                    placeholder="Email"
+                    autoComplete="email"
+                    value={this.state.emailUser}
+                    onChange={this.onChange}
+                  />
+                </Col>
+              </FormGroup>
 
-                                <FormGroup row>
-                                    <Label sm={2}>Password</Label>
+              <FormGroup row>
+                <Label sm={2}>Password</Label>
 
-                                    <Col sm={10}>
-                                        <Input
-                                        name="passwordUser"
-                                        id="passwordUser"
-                                        type="password"
-                                        placeholder="Password"
-                                        autoComplete="new-password"
-                                        onChange={this.onChange}
+                <Col sm={10}>
+                  <Input
+                    name="passwordUser"
+                    id="passwordUser"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="new-password"
+                    value={this.state.passwordUser}
+                    onChange={this.onChange}
+                  />
+                </Col>
+              </FormGroup>
+            </Form>
+          </ModalBody>
 
-                                        />
-                                    </Col>
-                                </FormGroup>
-
-                            </Form>
-                        </ModalBody>
-
-                        <ModalFooter>
-                            <Button color="success" type="submit">Register</Button>
-                        </ModalFooter>
-                    </Modal>
-                </div>
-            </div>
-         );
-    }
+          <ModalFooter>
+            <Button
+              color="success"
+              type="submit"
+              onClick={this.modalButtonRegister}
+            >
+              Register
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 const buttonStyle = {
-    color: "#fff"
-  };
- 
+  color: "#fff"
+};
+
 export default Register;
